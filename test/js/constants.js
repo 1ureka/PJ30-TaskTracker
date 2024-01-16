9486535d2c07696d0a6d7bf6e94794c73fa0365b
1ureka <summerheadquarters001@gmail.com>
2024-01-16 16:32:10 +0800

    開發task element準備工作、移動全域變數至單獨檔案

---
+++ test/js/constants.js
@@ -0,0 +1,27 @@
+const CATEGORISE = [
+  "未分類",
+  "PJ24",
+  "PJ25",
+  "PJ26",
+  "PJ27",
+  "PJ28",
+  "PJ29",
+  "PJ30",
+];
+
+const COLORMAP = {
+  U: "#bbb",
+  S: "#92e9ff",
+  O: "#8ce197",
+  F: "#ea81af",
+  未分類: "#bbb",
+  PJ24: "#ffff7a",
+  PJ25: "#8ce197",
+  PJ26: "#8ce197",
+  PJ27: "#92e9ff",
+  PJ28: "#92e9ff",
+  PJ29: "#ea81af",
+  PJ30: "#ea81af",
+};
+
+const STATUSMAP = { U: "未完成", S: "跳過", O: "完成", F: "失敗" };
