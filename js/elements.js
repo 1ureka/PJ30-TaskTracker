26a453ff1047fa18c5a9070f5d8d83dcefd73dd8
1ureka <summerheadquarters001@gmail.com>
2024-03-03 08:43:06 +0800

    刪除功能內部化

--- js/elements.js
+++ js/elements.js
@@ -1189,11 +1189,6 @@ class Task {
     const container = $("<div>").addClass("task-container");
     container.data("info", JSON.stringify(config));
 
-    // 刪除模式
-    const deleteIcon = new DeleteIconB();
-    deleteIcon.appendTo(container);
-    deleteIcon.addClass("task-delete-icon");
-
     // 上半部容器
     const infoContainer = $("<div>").addClass("task-info-container");
     const tagsContainer = $("<div>").addClass("task-tags-container");
@@ -1232,18 +1227,50 @@ class Task {
     return [
       $("<button>")
         .addClass("task-edit-button")
-        .append($("<img>").attr("src", "icons/pencil.png")),
+        .append(
+          $("<img>").attr("src", "icons/pencil.png"),
+          $("<span>").addClass("tip").text("編輯")
+        ),
       $("<button>")
         .addClass("task-copy-button")
         .append(
           $("<img>").attr("src", "icons/copy (left).png"),
-          $("<img>").attr("src", "icons/copy (right).png")
+          $("<img>").attr("src", "icons/copy (right).png"),
+          $("<span>").addClass("tip").text("複製")
         ),
+      $("<button>")
+        .addClass("task-delete-button")
+        .append(this._createSVG(), $("<span>").addClass("tip").text("刪除")),
     ];
   }
 
+  _createSVG() {
+    return $(`
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 50 59"
+    ><path
+      fill="#B5BAC1"
+      d="M0 7.5C0 5.01472 2.01472 3 4.5 3H45.5C47.9853 3 50 5.01472 50 7.5V7.5C50 8.32843 49.3284 9 48.5 9H1.5C0.671571 9 0 8.32843 0 7.5V7.5Z"
+    ></path>
+    <path
+      fill="#B5BAC1"
+      d="M17 3C17 1.34315 18.3431 0 20 0H29.3125C30.9694 0 32.3125 1.34315 32.3125 3V3H17V3Z"
+    ></path>
+    <path
+      fill="#B5BAC1"
+      d="M2.18565 18.0974C2.08466 15.821 3.903 13.9202 6.18172 13.9202H43.8189C46.0976 13.9202 47.916 15.821 47.815 18.0975L46.1699 55.1775C46.0751 57.3155 44.314 59.0002 42.1739 59.0002H7.8268C5.68661 59.0002 3.92559 57.3155 3.83073 55.1775L2.18565 18.0974ZM18.0003 49.5402C16.6196 49.5402 15.5003 48.4209 15.5003 47.0402V24.9602C15.5003 23.5795 16.6196 22.4602 18.0003 22.4602C19.381 22.4602 20.5003 23.5795 20.5003 24.9602V47.0402C20.5003 48.4209 19.381 49.5402 18.0003 49.5402ZM29.5003 47.0402C29.5003 48.4209 30.6196 49.5402 32.0003 49.5402C33.381 49.5402 34.5003 48.4209 34.5003 47.0402V24.9602C34.5003 23.5795 33.381 22.4602 32.0003 22.4602C30.6196 22.4602 29.5003 23.5795 29.5003 24.9602V47.0402Z"
+      clip-rule="evenodd"
+      fill-rule="evenodd"
+    ></path>
+    <path fill="#B5BAC1" d="M2 13H48L47.6742 21.28H2.32031L2 13Z"></path>
+    </svg>
+    `);
+  }
+
   _bindDeleteEvents() {
-    this.element.on("click", ".task-delete-icon", async () => {
+    this.element.on("click", ".task-delete-button", async () => {
       gsap.to(this.element, {
         duration: 0.1,
         ease: "set1",
@@ -1512,13 +1539,41 @@ class Separator {
    * @returns {jQuery} - 創建的分隔線元素的 jQuery 物件。
    */
   _create() {
-    const separator = $("<div>").addClass("separator");
-
-    this._deleteIcon = new DeleteIconB();
-    this._deleteIcon.appendTo(separator);
-    this._deleteIcon.addClass("task-delete-icon");
+    const separator = $("<div>")
+      .addClass("separator")
+      .append(
+        $("<button>").append(
+          this._createSVG(),
+          $("<span>").addClass("tip").text("刪除")
+        )
+      );
 
     return separator;
+  }
+
+  _createSVG() {
+    return $(`
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 50 59"
+    ><path
+      fill="#B5BAC1"
+      d="M0 7.5C0 5.01472 2.01472 3 4.5 3H45.5C47.9853 3 50 5.01472 50 7.5V7.5C50 8.32843 49.3284 9 48.5 9H1.5C0.671571 9 0 8.32843 0 7.5V7.5Z"
+    ></path>
+    <path
+      fill="#B5BAC1"
+      d="M17 3C17 1.34315 18.3431 0 20 0H29.3125C30.9694 0 32.3125 1.34315 32.3125 3V3H17V3Z"
+    ></path>
+    <path
+      fill="#B5BAC1"
+      d="M2.18565 18.0974C2.08466 15.821 3.903 13.9202 6.18172 13.9202H43.8189C46.0976 13.9202 47.916 15.821 47.815 18.0975L46.1699 55.1775C46.0751 57.3155 44.314 59.0002 42.1739 59.0002H7.8268C5.68661 59.0002 3.92559 57.3155 3.83073 55.1775L2.18565 18.0974ZM18.0003 49.5402C16.6196 49.5402 15.5003 48.4209 15.5003 47.0402V24.9602C15.5003 23.5795 16.6196 22.4602 18.0003 22.4602C19.381 22.4602 20.5003 23.5795 20.5003 24.9602V47.0402C20.5003 48.4209 19.381 49.5402 18.0003 49.5402ZM29.5003 47.0402C29.5003 48.4209 30.6196 49.5402 32.0003 49.5402C33.381 49.5402 34.5003 48.4209 34.5003 47.0402V24.9602C34.5003 23.5795 33.381 22.4602 32.0003 22.4602C30.6196 22.4602 29.5003 23.5795 29.5003 24.9602V47.0402Z"
+      clip-rule="evenodd"
+      fill-rule="evenodd"
+    ></path>
+    <path fill="#B5BAC1" d="M2 13H48L47.6742 21.28H2.32031L2 13Z"></path>
+    </svg>
+    `);
   }
 
   /**
@@ -1527,7 +1582,7 @@ class Separator {
    * @returns {Separator} - Separator 類別的實例。
    */
   _bindDeleteEvents() {
-    this._deleteIcon.elements[0].on("click", async () => {
+    this.element.on("click", "button", async () => {
       gsap.to(this.element, {
         duration: 0.1,
         ease: "set1",
